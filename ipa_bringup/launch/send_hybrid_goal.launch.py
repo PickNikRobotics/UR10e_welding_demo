94b9c0e2cac45da3b734ea3ffbf6b9e17383ae4d
AdamPettinger <adam.pettinger@utexas.edu>
2021-08-12 19:44:20 +0000

    Fix clang...

--- ipa_bringup/launch/send_hybrid_goal.launch.py
+++ ipa_bringup/launch/send_hybrid_goal.launch.py
@@ -31,6 +31,7 @@ from launch_ros.actions import Node, ComposableNodeContainer
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.descriptions import ComposableNode
 
+
 def load_yaml(package_name, file_path):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
@@ -41,248 +42,249 @@ def load_yaml(package_name, file_path):
     except OSError:  # parent of IOError, OSError *and* WindowsError where available
         return None
 
+
 def generate_launch_description():
-  declared_arguments = []
-  # UR specific arguments
-  declared_arguments.append(
-      DeclareLaunchArgument(
+    declared_arguments = []
+    # UR specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
             "ur_type",
             default_value="ur10e",
             description="Type/series of used UR robot.",
         )
-  )
-  # TODO(anyone): enable this when added into ROS2-foxy
-  # choices=['ur3', 'ur3e', 'ur5', 'ur5e', 'ur10', 'ur10e', 'ur16e']))
-  declared_arguments.append(
-      DeclareLaunchArgument(
+    )
+    # TODO(anyone): enable this when added into ROS2-foxy
+    # choices=['ur3', 'ur3e', 'ur5', 'ur5e', 'ur10', 'ur10e', 'ur16e']))
+    declared_arguments.append(
+        DeclareLaunchArgument(
             "robot_ip",
             default_value="yyy.yyyy.yyyy.yyyy",
             description="IP address by which the robot can be reached.",
         )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "safety_limits",
-          default_value="true",
-          description="Enables the safety limits controller if true.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "safety_pos_margin",
-          default_value="0.15",
-          description="The margin to lower and upper limits in the safety controller.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "safety_k_position",
-          default_value="20",
-          description="k-position factor in the safety controller.",
-      )
-  )
-  # General arguments
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "description_package",
-          default_value="ipa_demo_cell_description",
-          description="Description package with robot URDF/XACRO files. Usually the argument \
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_pos_margin",
+            default_value="0.15",
+            description="The margin to lower and upper limits in the safety controller.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_k_position",
+            default_value="20",
+            description="k-position factor in the safety controller.",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="ipa_demo_cell_description",
+            description="Description package with robot URDF/XACRO files. Usually the argument \
       is not set, it enables use of a custom description.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "description_file",
-          default_value="ipa_ur_demo.urdf.xacro",
-          description="URDF/XACRO description file with the robot.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "moveit_config_package",
-          default_value="ipa_moveit_config",
-          description="MoveIt config package with robot SRDF/XACRO files. Usually the argument \
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="ipa_ur_demo.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "moveit_config_package",
+            default_value="ipa_moveit_config",
+            description="MoveIt config package with robot SRDF/XACRO files. Usually the argument \
       is not set, it enables use of a custom moveit config.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "moveit_config_file",
-          default_value="ur.srdf.xacro",
-          description="MoveIt SRDF/XACRO description file with the robot.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "prefix",
-          default_value='""',
-          description="Prefix of the joint names, useful for \
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "moveit_config_file",
+            default_value="ur.srdf.xacro",
+            description="MoveIt SRDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
       multi-robot setup. If changed than also joint names in the controllers' configuration \
       have to be updated.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "use_fake_hardware",
-          default_value="false",
-          description="Start robot with fake hardware mirroring command to its states.",
-      )
-  )
-  declared_arguments.append(
-      DeclareLaunchArgument(
-          "fake_sensor_commands",
-          default_value="false",
-          description="Enable fake command interfaces for sensors used for simple simulations. \
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
           Used only if 'use_fake_hardware' parameter is true.",
-      )
-  )
+        )
+    )
 
-  # Initialize Arguments
-  ur_type = LaunchConfiguration("ur_type")
-  robot_ip = LaunchConfiguration("robot_ip")
-  safety_limits = LaunchConfiguration("safety_limits")
-  safety_pos_margin = LaunchConfiguration("safety_pos_margin")
-  safety_k_position = LaunchConfiguration("safety_k_position")
-  # General arguments
-  description_package = LaunchConfiguration("description_package")
-  description_file = LaunchConfiguration("description_file")
-  moveit_config_package = LaunchConfiguration("moveit_config_package")
-  moveit_config_file = LaunchConfiguration("moveit_config_file")
-  prefix = LaunchConfiguration("prefix")
-  use_fake_hardware = LaunchConfiguration("use_fake_hardware")
-  fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+    # Initialize Arguments
+    ur_type = LaunchConfiguration("ur_type")
+    robot_ip = LaunchConfiguration("robot_ip")
+    safety_limits = LaunchConfiguration("safety_limits")
+    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
+    safety_k_position = LaunchConfiguration("safety_k_position")
+    # General arguments
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    moveit_config_package = LaunchConfiguration("moveit_config_package")
+    moveit_config_file = LaunchConfiguration("moveit_config_file")
+    prefix = LaunchConfiguration("prefix")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
 
-  joint_limit_params = PathJoinSubstitution(
-      [FindPackageShare(description_package), "config", ur_type, "joint_limits.yaml"]
-  )
-  kinematics_params = PathJoinSubstitution(
-      [
-          FindPackageShare(description_package),
-          "config",
-          ur_type,
-          "calibrated_kinematics.yaml",
-      ]
-  )
-  physical_params = PathJoinSubstitution(
-      [
-          FindPackageShare(description_package),
-          "config",
-          ur_type,
-          "physical_parameters.yaml",
-      ]
-  )
-  visual_params = PathJoinSubstitution(
-      [
-          FindPackageShare(description_package),
-          "config",
-          ur_type,
-          "visual_parameters.yaml",
-      ]
-  )
-  script_filename = PathJoinSubstitution(
-      [FindPackageShare("ur_robot_driver"), "resources", "ros_control.urscript"]
-  )
-  input_recipe_filename = PathJoinSubstitution(
-      [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
-  )
-  output_recipe_filename = PathJoinSubstitution(
-      [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
-  )
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [
+            FindPackageShare(description_package),
+            "config",
+            ur_type,
+            "calibrated_kinematics.yaml",
+        ]
+    )
+    physical_params = PathJoinSubstitution(
+        [
+            FindPackageShare(description_package),
+            "config",
+            ur_type,
+            "physical_parameters.yaml",
+        ]
+    )
+    visual_params = PathJoinSubstitution(
+        [
+            FindPackageShare(description_package),
+            "config",
+            ur_type,
+            "visual_parameters.yaml",
+        ]
+    )
+    script_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "ros_control.urscript"]
+    )
+    input_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
+    )
+    output_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
+    )
 
-  robot_description_content = Command(
-      [
-          PathJoinSubstitution([FindExecutable(name="xacro")]),
-          " ",
-          PathJoinSubstitution(
-              [FindPackageShare(description_package), "urdf", description_file]
-          ),
-          " ",
-          "robot_ip:=",
-          robot_ip,
-          " ",
-          "joint_limit_params:=",
-          joint_limit_params,
-          " ",
-          "kinematics_params:=",
-          kinematics_params,
-          " ",
-          "physical_params:=",
-          physical_params,
-          " ",
-          "visual_params:=",
-          visual_params,
-          " ",
-          "safety_limits:=",
-          safety_limits,
-          " ",
-          "safety_pos_margin:=",
-          safety_pos_margin,
-          " ",
-          "safety_k_position:=",
-          safety_k_position,
-          " ",
-          "name:=",
-          ur_type,
-          " ",
-          "script_filename:=",
-          script_filename,
-          " ",
-          "input_recipe_filename:=",
-          input_recipe_filename,
-          " ",
-          "output_recipe_filename:=",
-          output_recipe_filename,
-          " ",
-          "prefix:=",
-          prefix,
-          " ",
-          "use_fake_hardware:=",
-          use_fake_hardware,
-          " ",
-          "fake_sensor_commands:=",
-          fake_sensor_commands,
-          " ",
-      ]
-  )
-  robot_description = {"robot_description": robot_description_content}
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", description_file]
+            ),
+            " ",
+            "robot_ip:=",
+            robot_ip,
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+            "safety_limits:=",
+            safety_limits,
+            " ",
+            "safety_pos_margin:=",
+            safety_pos_margin,
+            " ",
+            "safety_k_position:=",
+            safety_k_position,
+            " ",
+            "name:=",
+            ur_type,
+            " ",
+            "script_filename:=",
+            script_filename,
+            " ",
+            "input_recipe_filename:=",
+            input_recipe_filename,
+            " ",
+            "output_recipe_filename:=",
+            output_recipe_filename,
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+            "fake_sensor_commands:=",
+            fake_sensor_commands,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
-  # MoveIt Configuration
-  robot_description_semantic_content = Command(
-      [
-          PathJoinSubstitution([FindExecutable(name="xacro")]),
-          " ",
-          PathJoinSubstitution(
-              [FindPackageShare(moveit_config_package), "srdf", moveit_config_file]
-          ),
-          " ",
-          "name:=",
-          # Also ur_type parameter could be used but then the planning group names in yaml
-          # configs has to be updated!
-          "ur",
-          " ",
-          "prefix:=",
-          prefix,
-          " ",
-      ]
-  )
-  robot_description_semantic = {
-      "robot_description_semantic": robot_description_semantic_content
-  }
+    # MoveIt Configuration
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(moveit_config_package), "srdf", moveit_config_file]
+            ),
+            " ",
+            "name:=",
+            # Also ur_type parameter could be used but then the planning group names in yaml
+            # configs has to be updated!
+            "ur",
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+        ]
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_content
+    }
 
-  kinematics_yaml = load_yaml("ipa_moveit_config", "config/kinematics.yaml")
-  robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
+    kinematics_yaml = load_yaml("ipa_moveit_config", "config/kinematics.yaml")
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
 
-  # Test Node
-  test_request_node = Node(
-      package="hybrid_planning_demo",
-      executable="hybrid_planning_test_node",
-      name="hybrid_planning_test_node",
-      output="screen",
-      parameters=[robot_description, robot_description_semantic, kinematics_yaml],
-  )
+    # Test Node
+    test_request_node = Node(
+        package="hybrid_planning_demo",
+        executable="hybrid_planning_test_node",
+        name="hybrid_planning_test_node",
+        output="screen",
+        parameters=[robot_description, robot_description_semantic, kinematics_yaml],
+    )
 
-  nodes_to_start = [
-      test_request_node,
-  ]
+    nodes_to_start = [
+        test_request_node,
+    ]
 
-  return LaunchDescription(declared_arguments + nodes_to_start)
+    return LaunchDescription(declared_arguments + nodes_to_start)
